8ac6026d9763619c8e42d0a49a195826f4a582e4
Sahil Meshram <sahilmeshram222@gmail.com>
2025-07-06 18:09:07 +0530

    api route changed

--- Frontend/src/lib/api.js
+++ Frontend/src/lib/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 //added api
-const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? `${import.meta.env.VITE_API_URL}` 
-    : 'http://localhost:5000/api'; 
+const API_BASE_URL = import.meta.env.VITE_API_URL
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
